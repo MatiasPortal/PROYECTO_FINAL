a54ab3f669a1d05daca39cde50db387114c5f923
Matias Portal <portalmatias4@gmail.com>
2023-09-19 14:48:09 -0300

    cambios proyecto final

--- src/dao/models/tickets.model.js
+++ src/dao/models/tickets.model.js
@@ -7,7 +7,7 @@ const schema = new mongoose.Schema({
     purchase_datetime: { type: Date, required: true, default: Date.now },
     amount: { type: Number, required: true },
     purchaser: { type: String, required: true },
-    products: [{ type: String, required: true }]
+    products: { type: String, required: true }
 });
 
 const ticketModel = mongoose.model(collection, schema);
